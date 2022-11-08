c33a9010abfea476ed2500bb7dacbabdc16c36b9
thejamiegc <jcallan32@gmail.com>
2022-11-08 13:29:57 +0100

    adds sql bottom and topping code to resoures

---
+++ src/main/resources/bottoms.sql
@@ -0,0 +1,5 @@
+INSERT INTO cupcake.bottom (bottom, bottomPrice) VALUES ('Chocolate', 5);
+INSERT INTO cupcake.bottom (bottom, bottomPrice) VALUES ('Vanilla', 5);
+INSERT INTO cupcake.bottom (bottom, bottomPrice) VALUES ('Nutmeg', 5);
+INSERT INTO cupcake.bottom (bottom, bottomPrice) VALUES ('Pistacio', 6);
+INSERT INTO cupcake.bottom (bottom, bottomPrice) VALUES ('Almond', 7);
---
+++ src/main/resources/toppings.sql
@@ -0,0 +1,9 @@
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Chocolate', 5);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Blueberry', 5);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Raspberry', 5);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Crispy', 6);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Strawberry', 6);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Rum/Raisin', 7);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Orange', 8);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Lemon', 8);
+INSERT INTO cupcake.topping (topping, toppingPrice) VALUES ('Blue cheese', 9);
