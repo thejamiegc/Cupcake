9fcb97fb40e60de5c928821fd40611a01c0d7b6b
thejamiegc <jcallan32@gmail.com>
2022-11-10 11:12:45 +0100

    fix my sql

--- src/main/resources/mysql_cupcake.sql
+++ src/main/resources/mysql_cupcake.sql
@@ -14,7 +14,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,N
 -- -----------------------------------------------------
 -- Schema cupcake
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+ CREATE SCHEMA IF NOT EXISTS `cupcake` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
 USE `cupcake` ;
 
 -- -----------------------------------------------------
