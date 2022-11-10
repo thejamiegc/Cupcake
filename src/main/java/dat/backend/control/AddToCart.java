24c500897ddae017e20e2a140fe5dec8229662c0
Helena Lykstoft <helena.botn.lykstoft@gmail.com>
2022-11-10 12:39:00 +0100

    adds AddToCart servlet

--- src/main/java/dat/backend/control/AddToCart.java
+++ src/main/java/dat/backend/control/AddToCart.java
@@ -1,5 +1,6 @@
 package dat.backend.control;
 
+import dat.backend.model.entities.Cupcake;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.UserFacade;
@@ -18,24 +19,18 @@ public class AddToCart extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        response.setContentType("text/html");
         HttpSession session = request.getSession();
-        session.setAttribute("user", null); // invalidating user object in session scope
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
+        ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
 
-        try
-        {
-            User user = UserFacade.login(username, password, connectionPool);
-            session = request.getSession();
-            session.setAttribute("user", user); // adding user object to session scope
-            request.getRequestDispatcher("WEB-INF/welcome.jsp").forward(request, response);
-        }
-        catch (DatabaseException e)
-        {
-            request.setAttribute("errormessage", e.getMessage());
-            request.getRequestDispatcher("error.jsp").forward(request, response);
-        }
+        int topId = Integer.parseInt(request.getParameter("top"));
+        int bottomId = Integer.parseInt(request.getParameter("bottom"));
+        int amount = Integer.parseInt(request.getParameter("amount"));
+
+        Cupcake cupcake = new Cupcake(topId,bottomId,amount);
+        cart.add(cupcake);
+        session.setAttribute("cart", cart);
+        request.setAttribute("cartsize",cart.getNumberOfCupcakes());
+
+        request.getRequestDispatcher("WEB-INF/shoppingcart.jsp").forward(request,response);
     }
 }
