52231905de34c4f6182685900c98cd34d1856d2b
thejamiegc <jcallan32@gmail.com>
2022-11-16 14:56:54 +0100

    fixes lists in createuser

--- src/main/java/dat/backend/control/CreateUser.java
+++ src/main/java/dat/backend/control/CreateUser.java
@@ -1,16 +1,20 @@
 package dat.backend.control;
 
 import dat.backend.model.config.ApplicationStart;
+import dat.backend.model.entities.Bottom;
 import dat.backend.model.entities.ShoppingCart;
+import dat.backend.model.entities.Topping;
 import dat.backend.model.entities.User;
 import dat.backend.model.exceptions.DatabaseException;
 import dat.backend.model.persistence.ConnectionPool;
+import dat.backend.model.persistence.CupcakeFacade;
 import dat.backend.model.persistence.UserFacade;
 
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(name = "CreateUser", value = "/createuser")
 public class CreateUser extends HttpServlet {
@@ -39,6 +43,11 @@ public class CreateUser extends HttpServlet {
             User user = UserFacade.login(username, password, connectionPool);
             session = request.getSession();
             session.setAttribute("user", user); // adding user object to session scope
+            List<Bottom> bottomList = CupcakeFacade.getBottoms(connectionPool);
+            session.setAttribute("bottomList", bottomList);
+
+            List<Topping> toppingList = CupcakeFacade.getToppings(connectionPool);
+            session.setAttribute("toppingList", toppingList);
             ShoppingCart cart = new ShoppingCart();
             session.setAttribute("cart", cart);
 
